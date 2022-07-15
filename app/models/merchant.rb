750114d5f1ce91679ca84631c1332f87b6363fbb
Ian Ross <ianrossdrums@gmail.com>
2022-07-14 18:04:43 -0600

    feat: create model methods

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,3 +1,9 @@
 class Merchant < ApplicationRecord
   has_many :items
+
+  def self.find_merchant(searched_name)
+    where("name ILIKE ?", "%#{searched_name}%")
+    .order(:name)
+    .first
+  end
 end
