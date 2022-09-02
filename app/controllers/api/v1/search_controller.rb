8dbcb810b9d31fdbf78885caab3df6e7ed9a5079
Ian Ross <ianrossdrums@gmail.com>
2022-09-01 18:01:24 -0600

    all postman test pass

--- app/controllers/api/v1/search_controller.rb
+++ app/controllers/api/v1/search_controller.rb
@@ -5,21 +5,13 @@ class Api::V1::SearchController < ApplicationController
       render json: MerchantSerializer.format_merchant(merchant)
     else
       render json: {
-        data: []
+        data: {}
         }
     end
   end
 
   def index
-    # if params[:name] && (params[:min_price]! || params[:min_price]!)
     items = Item.find_items(params[:name])
     render json: ItemSerializer.format_items(items)
-    # elsif params[:min_price]
-    #
-    # elsif params[:max_price]
-    #
-    # else
-    #   render status: 404
-    # end
   end
 end
